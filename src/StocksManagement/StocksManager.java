2ce653aaea564c1f1cf175e3a8bc6789ec91ed04
gyuyoung2 <79908225+gyuyoung2@users.noreply.github.com>
2021-05-16 12:18:43 +0900

    Add Exception and Refactoring

--- src/StocksManagement/StocksManager.java
+++ src/StocksManagement/StocksManager.java
@@ -1,6 +1,7 @@
 package StocksManagement;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import stock.KoreaStock;
@@ -20,7 +21,8 @@ public class StocksManager {
 	public void addStocks(){ //주식'종목 정보'추가하는 함수 생성
 		int kind = 0; //사용자가 메뉴 입력할 변수 선언과 초기화
 		StockInput stockInput; //Stock 클래스 생성자 생성
-		while(kind != 1 && kind != 2) { //사용자가 1,2 이외에 숫자를 입력할때까지 반복
+		while(kind != 1 || kind != 2) { //사용자가 1,2 이외에 숫자를 입력할때까지 반복
+			try {
 			System.out.print("1. 국내주식"); //국내주식 종목 추가 안내
 			System.out.println(" 2. 해외주식"); //해외주식 종목 추가 안내
 			System.out.print("주식종류를 선택해주세요 between 1 - 2: "); //사용자에게 종목종류를 입력받을려고 안내문 출력
@@ -41,11 +43,25 @@ public class StocksManager {
 				System.out.print("주식종류를 선택해주세요 1 or 2: "); //사용자에게 올바른 값을 입력할때까지 출력
 			}
 		}
+			catch(InputMismatchException e) { //try/catch문을 통해 예외처리
+				System.out.println("Please put an integer between 1 and 2f!"); //오류 안내문 출력
+				if(input.hasNext()) { 
+					input.next();
+				}
+				kind = -1;
+			}
+		}
 	}
 	
 	public void deleteStocks(){ //주식'종목 정보'삭제하는 함수 생성
 		System.out.println("어떤 종목을 삭제하시겠습니까?"); // 사용자에게 삭제 할 종목 입력 안내문 출력
 		String name = input.next(); //삭제할 종목명 입력값을 name에 할당
+		int index = findIndex(name); //findIndex method에서 반환한 index값을 index변수에 저장
+		removefromStocks(index, name); //removefromStocks method를 통해 해당 index데이터 삭제
+		
+	}
+	
+	public int findIndex(String name) { //findIndex method 생성
 		int index = -1; //array가 index를 못찾는것을 예상 하기 위해 index에 -1 할당.
 		for(int i = 0; i<stocks.size(); i++) { //name배열에 입력된 크기만큼 for반복문 실행하게 loop설계
 			if(stocks.get(i).getStocksName().equals(name)) { //입력한 종목이 내가 저장한 종목가 같으면 index값 할당
@@ -53,16 +69,19 @@ public class StocksManager {
 				break; //종료
 			}
 		}
-			
+		return index; //index값 반환
+	}
+	
+	public int removefromStocks(int index, String name) { //removefromStocks method 생성
 		if(index >= 0) { //index값이 0이상
-				stocks.remove(index); //배열 index값 정보 삭제
-				System.out.println("입력된 종목 " + name + "이(가) 삭제되었습니다."); //사용자가 입력한 종목 삭제 안내문 출력
-			} 
-		else { //index값이 음수일경우
-				System.out.println("입력된 종목은 정보가 없습니다."); //사용자에게 입력한 정보 없음 안내문 출력
-				return;
-			}
-		
+			stocks.remove(index); //배열 index값 정보 삭제
+			System.out.println("입력된 종목 " + name + "이(가) 삭제되었습니다."); //사용자가 입력한 종목 삭제 안내문 출력
+			return 1;
+		} 
+	else { //index값이 음수일경우
+			System.out.println("입력된 종목은 정보가 없습니다."); //사용자에게 입력한 정보 없음 안내문 출력
+			return -1;
+		}
 	}
 	
 	
@@ -70,38 +89,29 @@ public class StocksManager {
 		System.out.print("종목 이름: "); //사용자에게 수정할 주식 종목 입력 안내문 출력
 		String name = input.next(); //사용자가 입력한 값을 name에 할당
 		for(int i = 0; i<stocks.size(); i++) { //name배열에 입력된 크기만큼 for반복문 실행하게 loop설계
-			StockInput stockInput = stocks.get(i); //stocks저장공간에 있는 인덱스 값을 stock에 할당
-			if(stockInput.getStocksName().equals(name)) { //만약 입력값과 현재 저장되어있는 주식종목 명이 값을 경우 문자열이므로 equals() method를 활용 
+			StockInput stock = stocks.get(i); //stocks저장공간에 있는 인덱스 값을 stock에 할당
+			if(stock.getStocksName().equals(name)) { //만약 입력값과 현재 저장되어있는 주식종목 명이 값을 경우 문자열이므로 equals() method를 활용 
 				int num = -1; // 사용자가 메뉴 입력할 변수 선언과 초기화
 				while (num != 5) { //edit 매뉴 설정
-					System.out.println("### Stocks Info Edit Menu ###");
-					System.out.println("1. Edit StocksName");
-					System.out.println("2. Edit Price Stocks");
-					System.out.println("3. Edit Goal Price");
-					System.out.println("4. Edit Memo");
-					System.out.println("5. Exit");
-					System.out.print("Select one number between 1 - 5:");
+					showEditMenu(); //showEditMenu method를 통해 사용자에게 수정섹션 메뉴 노출
 					num = input.nextInt(); //사용자에게 입력받은 값을 num에 할당
 					
-				switch(num) { //switch문을 활용해 사용자 선택 용이
-					case 1: System.out.print("종목 이름: "); //사용자에게 종목이름을 입력 안내문 출력
-							String StocksName = input.next(); //입력한 종목이름을 StocksName에 할당
-							stockInput.setStocksName(StocksName); //그 입려된값을 Stock클래스에 있는 StocksName에 할당
+					switch(num) { //switch문을 활용해 사용자 선택 용이
+						case 1: 
+							stock.setStockName(input); //setStockName()을 통해 주식이름 수정
 							break;//종료
-					case 2: System.out.print("종목 매수가: "); //사용자에게 종목 매수가를 입력 안내문 출력
-							int buyStock = input.nextInt(); //입력한 매수가를 buyStock에 할당
-							stockInput.setBuyStock(buyStock); //그 입력된값을 Stock클래스에 있는 buyStock에 할당
+						case 2: 
+							stock.setStockPrice(input); //setStockPrice()을 통해 주식매수가 수정
 							break;//종료
-					case 3: System.out.print("종목 목표가: "); //사용자에게 종목 목표가를 입력 안내문 출력
-							int goalPrice = input.nextInt(); //입력한 목표가를 goalPrice에 할당
-							stockInput.setGoalPrice(goalPrice); //그 입력된값을 Stock클래스에 있는 goalPrice에 할당
+						case 3: 
+							stock.setStockGoal(input); //setStockGoal()을 통해 주식목표가 수정
 							break;//종료
-					case 4: System.out.print("종목 메모: "); //사용자에게 종목 메모를 입력 안내문 출력
-							String memo = input.next(); //입력한 목표가를 memo에 할당
-							stockInput.setmemo(memo); //그 입력된 값을 Stock클래스에 있는 memo에 할당
+						case 4: 
+							stock.setStockMemo(input); //setStockMemo()을 통해 주식메모 수정
 							break;//종료
-					case 5: continue; //loop문 탈출
-					} //swtich
+						default: 
+							continue; //loop문 탈출
+						} //swtich
 			    }break; //while //종료
 			}//if
 		}//for
@@ -121,6 +131,16 @@ public class StocksManager {
 
 	}
 	
+	
+	public void showEditMenu() { //showEditMenu method 생성
+		System.out.println("### Stocks Info Edit Menu ###");
+		System.out.println("1. Edit StocksName");
+		System.out.println("2. Edit Price Stocks");
+		System.out.println("3. Edit Goal Price");
+		System.out.println("4. Edit Memo");
+		System.out.println("5. Exit");
+		System.out.print("Select one number between 1 - 5:");
+	}
 	
 	
 }
