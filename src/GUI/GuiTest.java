73318030d141e4c52c960c5449550f1cbf77d727
gyuyoung2 <tkrhk2836@naver.com>
2021-05-29 15:35:46 +0900

    added GUI

--- src/GUI/GuiTest.java
+++ src/GUI/GuiTest.java
@@ -3,7 +3,7 @@ package GUI;
 public class GuiTest {
 
 	public static void main(String[] args) {
-		MenuSelection menuselction = new MenuSelection(); //MenuSelction 梓端 持失
+		MenuSelection menuselction = new MenuSelection(); //MenuSelction 梓端 持失.
 		StockAdder stockadder = new StockAdder(); //StockAdder 梓端 持失
 		StockViewer stockviewer = new StockViewer(); //StockViewer 梓端 持失
 	}
