e9a9188572a9cdad9ee73a14f1601a1d521928f0
gyuyoung2 <tkrhk2836@naver.com>
2021-06-01 15:13:48 +0900

    added AddListener & ViewListener

---
+++ src/Listener/ButtonViewListener.java
@@ -0,0 +1,27 @@
+package Listener;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import GUI.StockViewer;
+import GUI.WindowFrame;
+
+public class ButtonViewListener implements ActionListener {
+	
+	WindowFrame frame;
+
+	public ButtonViewListener(WindowFrame frame) {
+		this.frame = frame;
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton b = (JButton) e.getSource();
+		StockViewer viewer = frame.getStockviewer();
+		frame.setupPanel(viewer);
+	}
+
+}
